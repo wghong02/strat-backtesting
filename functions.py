b074291e52c3b6bfc919be1c33cc487fd063dc47
Gilbert <wghong@bu.edu>
2023-07-08 04:46:28 -0400

    added functions

--- functions.py
+++ functions.py
@@ -4,3 +4,36 @@ from dash import html
 from dash.dependencies import Input, Output
 import pandas as pd
 
+@app.callback(
+    Output('content', 'children'),
+    [Input('btn-1', 'n_clicks'),
+    Input('btn-2', 'n_clicks'),
+    Input('btn-3', 'n_clicks')]
+)
+def update_content(btn1, btn2, btn3):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        return "Select an option"
+    else:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+        if button_id == 'btn-2':
+            return html.Button('Add New Strategy', id='add-strategy', n_clicks=0)
+        else:
+            return f"You selected {button_id}"
+
+@app.callback(
+    Output('add-strategy', 'children'),
+    [Input('add-strategy', 'n_clicks')]
+)
+def add_strategy(n_clicks):
+    if n_clicks > 0:
+        return [
+            html.Div([
+                html.Div('Gain', style={'color': 'green'}),
+                html.Button('+ Gain', id='btn-gain', n_clicks=0)
+            ]),
+            html.Div([
+                html.Div('Loss', style={'color': 'red'}),
+                html.Button('- Loss', id='btn-loss', n_clicks=0)
+            ])
+        ]
--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from dash import html
 from dash.dependencies import Input, Output
 import pandas as pd
 
-import functions
-
 app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}],
 )
@@ -24,19 +22,7 @@ app.layout = html.Div([
     html.Div(id='content', style={'width': '80%', 'display': 'inline-block'})
 ])
 
-@app.callback(
-    Output('content', 'children'),
-    [Input('btn-1', 'n_clicks'),
-     Input('btn-2', 'n_clicks'),
-     Input('btn-3', 'n_clicks')]
-)
-def update_content(btn1, btn2, btn3):
-    ctx = dash.callback_context
-    if ctx.triggered:
-        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-        if button_id == 'btn-2':
-            return html.Button('Add New Strategy', id='add-strategy', n_clicks=0)
-        return f"You selected {button_id}"
+import functions
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='127.0.0.1', port=8080)
