74ad425e04f8c69a0aa2166f7224ba1faf100205
Gilbert <wghong@bu.edu>
2023-07-08 17:34:34 -0400

    added pnl

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import dash
 from dash import dcc
 from dash import html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import pandas as pd
 
 app = dash.Dash(
@@ -51,13 +51,37 @@ def add_strategy(n_clicks):
         return [
             html.Div([
                 html.Div('Gain', style={'color': 'green'}),
+                dcc.Input(id='input-gain', type='number', value=0),
                 html.Button('+ Gain', id='btn-gain', n_clicks=0)
             ]),
             html.Div([
                 html.Div('Loss', style={'color': 'red'}),
+                dcc.Input(id='input-loss', type='number', value=0),
                 html.Button('- Loss', id='btn-loss', n_clicks=0)
-            ])
+            ]),
+            html.Div(id='pnl', children='PnL: 0')
         ]
+    
+@app.callback(
+    Output('pnl', 'children'),
+    [Input('btn-gain', 'n_clicks'),
+     Input('btn-loss', 'n_clicks')],
+    [State('input-gain', 'value'),
+     State('input-loss', 'value'),
+     State('pnl', 'children')]
+)
+def update_pnl(n_clicks_gain, n_clicks_loss, gain, loss, pnl):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        return "PnL: 0"
+    else:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+        current_pnl = int(pnl.split(': ')[1])
+        if button_id == 'btn-gain':
+            return f"PnL: {current_pnl + gain}"
+        elif button_id == 'btn-loss':
+            return f"PnL: {current_pnl - loss}"
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='127.0.0.1', port=8080)
