f24b77b7c5416f0899c39f41e5d9606518f83d13
Gilbert <wghong@bu.edu>
2023-07-08 04:50:56 -0400

    modded

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ app.layout = html.Div([
         html.Button('Home', id='btn-1', n_clicks=0),
         html.Button('Strategies', id='btn-2', n_clicks=0),
         html.Button('History', id='btn-3', n_clicks=0),
-    ], style={'width': '120%', 'display': 'inline-block'}),
+    ], style={'width': '20%', 'display': 'inline-block', 'vertical-align': 'top'}),
     html.Div(id='content', style={'width': '80%', 'display': 'inline-block'})
 ])
 
 @app.callback(
     Output('content', 'children'),
     [Input('btn-1', 'n_clicks'),
-    Input('btn-2', 'n_clicks'),
-    Input('btn-3', 'n_clicks')]
+     Input('btn-2', 'n_clicks'),
+     Input('btn-3', 'n_clicks')]
 )
 def update_content(btn1, btn2, btn3):
     ctx = dash.callback_context
@@ -35,12 +35,15 @@ def update_content(btn1, btn2, btn3):
     else:
         button_id = ctx.triggered[0]['prop_id'].split('.')[0]
         if button_id == 'btn-2':
-            return html.Button('Add New Strategy', id='add-strategy', n_clicks=0)
+            return [
+                html.Button('Add New Strategy', id='add-strategy', n_clicks=0),
+                html.Div(id='strategy-content')
+            ]
         else:
             return f"You selected {button_id}"
 
 @app.callback(
-    Output('add-strategy', 'children'),
+    Output('strategy-content', 'children'),
     [Input('add-strategy', 'n_clicks')]
 )
 def add_strategy(n_clicks):
